368e7b0135f9679af4121331a0ca08577234811d
pixellini <jake@pixellini.com>
2024-03-27 22:06:03 +1100

    set astronaut rotation

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './style.scss'
 import axios from 'axios'
 
+const MIN_ORBIT_SPEED = 100
+const MAX_ORBIT_SPEED = 200
 const SAMPLE_DATA = {
     "message": "success",
     "people": [
@@ -45,7 +47,6 @@ export async function fetchAstronauts () {
         //         'Access-Control-Allow-Origin': '*'
         //     }
         // })
-        console.log('astronauts', SAMPLE_DATA)
         return SAMPLE_DATA
     }
     catch(error) {
@@ -58,40 +59,63 @@ function getRandomNumber (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function generateAstronautElement () {
-  const size = getRandomNumber(220, 400)
-  const speed = getRandomNumber(150, 250)
-  const pos = getRandomNumber(0, 360)
+function generateAstronautElement (name, craft) {
+  const size = getRandomNumber(250, 500) + 'px'
+  const speed = getRandomNumber(MIN_ORBIT_SPEED, MAX_ORBIT_SPEED) + 's'
+  const pos = getRandomNumber(0, 360) + 'deg'
 
   return `
-    <div class="center" style="transform: rotate(${pos}deg);">
-      <div class="astronaut-container" style="width: ${size}px; height: ${size}px; animation: rotate ${speed}s linear infinite">
-        <div class="astronaut"></div>
+    <div class="center" style="transform: rotate(${pos})">
+      <div class="astronaut-container" style="width: ${size}; height: ${size}; animation-duration: ${speed};">
+        <div style="transform: rotate(-${pos})">
+          <div class="astronaut" style="animation-duration: ${speed}"></div>
+          <div class="astronaut-details hidden" style="animation-duration: ${speed}">
+            <span>${name}</span>
+          </div>
+        </div>
       </div>
     </div>
   `
 }
 
-const STAR_SIZE = 20 //px
 function generateStar () {
-  const posX = getRandomNumber(0, window.innerWidth - STAR_SIZE)
-  const posY = getRandomNumber(0, window.innerHeight - STAR_SIZE)
+  const posX = getRandomNumber(0, 100)
+  const posY = getRandomNumber(0, 100)
   return `
-    <div class="star" style="top: ${posY}px; left: ${posX}px"></div>
+    <div class="star" style="top: ${posY}%; left: ${posX}%"></div>
   `
+}
+
+function setAstronautHoverEvent () {
+  const astronautEls = document.querySelectorAll('.astronaut')
+  const astronautDetailEls = document.querySelectorAll('.astronaut-details')
+  // astronautEls.forEach((astronaut, index) => {
+  //   astronaut.addEventListener('mouseenter', () => {
+  //     astronautDetailEls[index].classList.remove('hidden')
+  //   })
+
+  //   astronaut.addEventListener('mouseleave', () => {
+  //     astronautDetailEls[index].classList.add('hidden')
+  //   })
+  // })
 }
 
 async function init () {
   const astronauts = await fetchAstronauts()
 
   const appEl = document.getElementById('app')
-  astronauts.people.forEach(asronaut => {
-    appEl.innerHTML += generateAstronautElement()
+  astronauts.people.forEach(astronaut => {
+    const { name, craft } = astronaut
+    if (name && craft) {
+      appEl.innerHTML += generateAstronautElement(name, craft)
+    }
   })
 
   for (let i = 0; i < 50; i++) {
     appEl.innerHTML += generateStar()
   }
+
+  setAstronautHoverEvent()
 }
 
 init()

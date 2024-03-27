03daf325a0a822265d17f99c14826791f0e4b320
pixellini <jake@pixellini.com>
2024-03-27 12:53:47 +1100

    initial commit - project setup

---
+++ js/fetch-astronauts.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export async function fetchAstronauts () {
+    console.log('fetching astronauts...')
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+import { fetchAstronauts } from './js/fetch-astronauts.js'
+
+function init () {
+  fetchAstronauts()
+}
+
+init()

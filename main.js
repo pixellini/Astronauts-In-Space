6e88b199401821b004fb51a971fdd730d65c8d5f
pixellini <jake@pixellini.com>
2024-03-29 14:15:45 +1100

    create constants and clean up

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import './style.scss'
 import axios from 'axios'
 
-const MIN_ORBIT_SPEED = 100
-const MAX_ORBIT_SPEED = 200
+const MIN_ORBIT_SPEED = 3
+const MAX_ORBIT_SPEED = 3
+const NUM_OF_STARS = 250
+const ASTRONAUT_ORBIT_MIN = 250
+const ASTRONAUT_ORBIT_MAX = 500 
 const SAMPLE_DATA = {
     "message": "success",
     "people": [
@@ -39,7 +42,6 @@ const SAMPLE_DATA = {
 }
 
 export async function fetchAstronauts () {
-
     try {
         // console.log('fetching astronauts...')
         // const result = await axios.get('http://api.open-notify.org/astros.json', {
@@ -56,66 +58,66 @@ export async function fetchAstronauts () {
 }
 
 function getRandomNumber (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function generateAstronautElement (name, craft) {
-  const size = getRandomNumber(250, 500) + 'px'
-  const speed = getRandomNumber(MIN_ORBIT_SPEED, MAX_ORBIT_SPEED) + 's'
-  const pos = getRandomNumber(0, 360) + 'deg'
+    const size = getRandomNumber(ASTRONAUT_ORBIT_MIN, ASTRONAUT_ORBIT_MAX) + 'px'
+    const speed = getRandomNumber(MIN_ORBIT_SPEED, MAX_ORBIT_SPEED) + 's'
+    const pos = getRandomNumber(0, 360) + 'deg'
 
-  return `
-    <div class="center" style="transform: rotate(${pos})">
-      <div class="astronaut-container" style="width: ${size}; height: ${size}; animation-duration: ${speed};">
-        <div style="transform: rotate(-${pos})">
-          <div class="astronaut" style="animation-duration: ${speed}"></div>
-          <div class="astronaut-details hidden" style="animation-duration: ${speed}">
-            <span>${name}</span>
-          </div>
+    return `
+        <div class="center" style="transform: rotate(${pos})">
+            <div class="astronaut-container" style="width: ${size}; height: ${size}; animation-duration: ${speed};">
+                <div style="transform: rotate(-${pos})">
+                    <div class="astronaut" style="animation-duration: ${speed}"></div>
+                    <div class="astronaut-details hidden" style="animation-duration: ${speed}">
+                        <span>${name}</span>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  `
+    `
 }
 
 function generateStar () {
-  const posX = getRandomNumber(0, 100)
-  const posY = getRandomNumber(0, 100)
-  return `
-    <div class="star" style="top: ${posY}%; left: ${posX}%"></div>
-  `
+    const posX = getRandomNumber(0, 100)
+    const posY = getRandomNumber(0, 100)
+    return `
+        <div class="star" style="top: ${posY}%; left: ${posX}%"></div>
+    `
 }
 
 function setAstronautHoverEvent () {
-  const astronautEls = document.querySelectorAll('.astronaut')
-  const astronautDetailEls = document.querySelectorAll('.astronaut-details')
-  // astronautEls.forEach((astronaut, index) => {
-  //   astronaut.addEventListener('mouseenter', () => {
-  //     astronautDetailEls[index].classList.remove('hidden')
-  //   })
+    const astronautEls = document.querySelectorAll('.astronaut')
+    const astronautDetailEls = document.querySelectorAll('.astronaut-details')
+    // astronautEls.forEach((astronaut, index) => {
+    //   astronaut.addEventListener('mouseenter', () => {
+    //     astronautDetailEls[index].classList.remove('hidden')
+    //   })
 
-  //   astronaut.addEventListener('mouseleave', () => {
-  //     astronautDetailEls[index].classList.add('hidden')
-  //   })
-  // })
+    //   astronaut.addEventListener('mouseleave', () => {
+    //     astronautDetailEls[index].classList.add('hidden')
+    //   })
+    // })
 }
 
 async function init () {
-  const astronauts = await fetchAstronauts()
+    const astronauts = await fetchAstronauts()
 
-  const appEl = document.getElementById('app')
-  astronauts.people.forEach(astronaut => {
-    const { name, craft } = astronaut
-    if (name && craft) {
-      appEl.innerHTML += generateAstronautElement(name, craft)
+    const appEl = document.getElementById('app')
+    astronauts.people.forEach(astronaut => {
+        const { name, craft } = astronaut
+        if (name && craft) {
+            appEl.innerHTML += generateAstronautElement(name, craft)
+        }
+    })
+
+    for (let i = 0; i < NUM_OF_STARS; i++) {
+        appEl.innerHTML += generateStar()
     }
-  })
 
-  for (let i = 0; i < 50; i++) {
-    appEl.innerHTML += generateStar()
-  }
-
-  setAstronautHoverEvent()
+    setAstronautHoverEvent()
 }
 
 init()
